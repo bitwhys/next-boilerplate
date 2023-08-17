0c6d1326ad34b5c7602bbc484259e1c95cf9c2c4
George Bockari <bitwhys@gmail.com>
2023-08-17 14:10:39 -0400

    refactor(navbar): use radix-themes components & change logo

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -6,6 +6,7 @@ import useScroll from "@/lib/hooks/use-scroll";
 import { useSignInModal } from "./sign-in-modal";
 import UserDropdown from "./user-dropdown";
 import { Session } from "next-auth";
+import {Button, Heading} from "@radix-ui/themes";
 
 export default function NavBar({ session }: { session: Session | null }) {
   const { SignInModal, setShowSignInModal } = useSignInModal();
@@ -22,26 +23,30 @@ export default function NavBar({ session }: { session: Session | null }) {
         } z-30 transition-all`}
       >
         <div className="mx-5 flex h-16 max-w-screen-xl items-center justify-between w-full">
-          <Link href="/" className="flex items-center font-display text-2xl">
-            <Image
-              src="/logo.png"
-              alt="Precedent logo"
-              width="30"
-              height="30"
-              className="mr-2 rounded-sm"
-            ></Image>
-            <p>Precedent</p>
+          <Link href="/" className="flex items-center gap-x-3 font-display text-2xl">
+            <svg className='h-8 w-8'  viewBox="0 0 640 640" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <rect width="640" height="640" rx="320" fill="#010101"/>
+              <g clip-path="url(#clip0_972_130)">
+                <path d="M300.888 190L470.763 359.188V450.015H391.488V391.839L268.106 268.954H242.475V450.015H163.2V190H300.888ZM391.488 280.234V190H470.763V280.234H391.488Z" fill="white"/>
+              </g>
+              <defs>
+                <clipPath id="clip0_972_130">
+                  <rect width="313.2" height="265.015" fill="white" transform="translate(163.2 187.5)"/>
+                </clipPath>
+              </defs>
+            </svg>
+            <Heading>Entry</Heading>
           </Link>
           <div>
             {session ? (
               <UserDropdown session={session} />
             ) : (
-              <button
-                className="rounded-full border border-black bg-black p-1.5 px-4 text-sm text-white transition-all hover:bg-white hover:text-black"
+              <Button
+                className="rounded-full"
                 onClick={() => setShowSignInModal(true)}
               >
                 Sign In
-              </button>
+              </Button>
             )}
           </div>
         </div>
